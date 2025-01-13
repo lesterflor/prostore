57eb35fbe42464de25ee7216fe39e88c6c358c01
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-13 16:12:11 -0500

    set theme for layout

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
 import Link from 'next/link';
 import Image from 'next/image';
+import ModeToggle from './mode-toggle';
 
 export default function Header() {
 	return (
@@ -25,6 +26,7 @@ export default function Header() {
 					</Link>
 				</div>
 				<div className='space-x-2'>
+					<ModeToggle />
 					<Button
 						asChild
 						variant='ghost'>
@@ -33,9 +35,7 @@ export default function Header() {
 						</Link>
 					</Button>
 
-					<Button
-						asChild
-						variant='ghost'>
+					<Button asChild>
 						<Link href='/sign-in'>
 							<UserIcon /> Sign In
 						</Link>
