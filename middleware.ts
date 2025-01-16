cb145ba63e828bb9aa6946f7e2dadf93c14139b0
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-16 14:09:08 -0500

    attempt lower bundle size for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,22 @@
-export { auth as middleware } from '@/auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function middleware(request: NextRequest) {
+	// clone request headers
+	const newRequestHeaders = new Headers(request.headers);
+
+	// create new response and add the new headers
+	const response = NextResponse.next({
+		request: {
+			headers: newRequestHeaders
+		}
+	});
+	// check for session cart cookie
+	if (!request.cookies.get('sessionCartId')) {
+		const sessionCartId = crypto.randomUUID();
+
+		// set newly generated session cart id in the response cookies
+		response.cookies.set('sessionCartId', sessionCartId);
+	}
+
+	return response;
+}
