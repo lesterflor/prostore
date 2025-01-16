da1a9ddadfbbbde864459476bfd8e19548374389
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-16 11:23:28 -0500

    cart page

--- lib/utils.ts
+++ lib/utils.ts
@@ -49,3 +49,19 @@ export function round2(value: number | string) {
 		throw new Error('value must be number or string');
 	}
 }
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat('en-ca', {
+	currency: 'CAD',
+	style: 'currency',
+	minimumFractionDigits: 2
+});
+
+export function formatCurrency(amount: number | string | null) {
+	if (typeof amount === 'number') {
+		return CURRENCY_FORMATTER.format(amount);
+	} else if (typeof amount === 'string') {
+		return CURRENCY_FORMATTER.format(Number(amount));
+	} else {
+		return 'NaN';
+	}
+}
