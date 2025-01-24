578333d6d8682f403d513cff8097b0757cd9502b
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-01-24 12:32:08 -0500

    promo components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,6 +6,8 @@ import {
 } from '@/lib/actions/products.actions';
 import ProductCarousel from '@/components/shared/product/product-carousel';
 import ViewAllProductsButton from '@/components/view-all-products-button';
+import IconBoxes from '@/components/icon-boxes';
+import DealCountdown from '@/components/deal-countdown';
 
 export default async function HomePage() {
 	const data = await getLatestProducts();
@@ -20,6 +22,8 @@ export default async function HomePage() {
 				limit={LATEST_PRODUCTS_LIMIT}
 			/>
 			<ViewAllProductsButton />
+			<DealCountdown />
+			<IconBoxes />
 		</>
 	);
 }
